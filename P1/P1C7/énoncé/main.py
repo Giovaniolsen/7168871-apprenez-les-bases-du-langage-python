6bf6d6fbbfd5d49505bee59d69ee564d4f10512e
Giovaniolsen <kindjiolsen83@gmail.com>
2026-07-06 17:09:15 +0100

    Add python P1C7 exercice

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits = {"Pomme":"rouge", "banane":"jaune", "orange":"orange"};
+fruits["kiwi"] = "vert";
+Couleur_banane = fruits["banane"];
+print(f"La couleur de la banane est : {Couleur_banane}");
+fruits["Pomme"] = "vert";
+del fruits["banane"];
+print(fruits.keys());
