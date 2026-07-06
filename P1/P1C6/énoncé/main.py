020446e9467dded4ad90c4521b31d44d07de7f8a
Giovaniolsen <kindjiolsen83@gmail.com>
2026-07-06 09:40:17 +0100

    Add P1C6 python exercice

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,8 @@
-# Écrivez votre code ici !
+fruits= ["pomme", "banane", "orange"];
+fruits.append("kiwi");
+fruits.remove("orange");
+fruits[1]="ananas";
+print(f"longueur de la liste : {len(fruits)}");
+fruits.sort();
+print("la liste de fruits triée est :", fruits);
+
